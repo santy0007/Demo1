db8ae2da1e06f01c196369f806eb9840c444e6e1
SANTOSH <santoshkumarpanda0007.com>
2019-10-22 19:07:45 +0530

    first commit

---
+++ stringProject.java
@@ -0,0 +1,24 @@
+package panda;
+
+public class stringProject {
+
+	public static void main(String[] args) {
+		System.out.println("Testing");
+
+	}
+	
+	public void oddeven()
+	{
+		int x=6;
+		if (x%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+		
+	}
+
+}
